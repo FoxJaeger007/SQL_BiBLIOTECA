91ffbd15a74909eeb1bbaa046212fcfeb44729b1
Andres Arias <87790546+FoxJaeger007@users.noreply.github.com>
2024-11-06 20:48:58 -0500

    Update Laboratorio_5.sql
    se crea la tabla cargo

--- Laboratorio_5.sql
+++ Laboratorio_5.sql
@@ -7,3 +7,9 @@ CREATE DATABASE Empresa;
 --se crea tabla empleados
   
 CREATE TABLE empleados ( id_empl INT PRIMARY KEY, nombre VARCHAR(50), cargo VARCHAR(50), salario DECIMAL(10, 2) );
+
+
+-- se crea tabla cargo
+
+USE empresa;
+CREATE TABLE cargo ( id_cargo INT PRIMARY KEY, nombre_cargo VARCHAR(50), descripcion VARCHAR(50), salario DECIMAL(10, 2) );
