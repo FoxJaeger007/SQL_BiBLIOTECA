3de0bdc28da654f6e102b26c0961f28fd1e9de2e
Andres Arias <87790546+FoxJaeger007@users.noreply.github.com>
2024-11-06 21:02:39 -0500

    Update Laboratorio_5.sql
    se realiza la inserción en las tablas

--- Laboratorio_5.sql
+++ Laboratorio_5.sql
@@ -13,3 +13,20 @@ CREATE TABLE empleados ( id_empl INT PRIMARY KEY, nombre VARCHAR(50), cargo VARC
 
 USE empresa;
 CREATE TABLE cargo ( id_cargo INT PRIMARY KEY, nombre_cargo VARCHAR(50), descripcion VARCHAR(50), salario DECIMAL(10, 2) );
+
+
+-- se realizan los ingresos a las tablas 
+
+
+USE empresa;
+INSERT INTO empleados (id_empl,nombre,cargo,salario) VALUES (1, 'Juan Pérez', 'Gerente', 7500000);
+INSERT INTO empleados (id_empl,nombre,cargo,salario) VALUES (2, 'Ana Gomez', 'Desarrollador', 6000000);
+INSERT INTO empleados (id_empl,nombre,cargo,salario) VALUES (3, 'Luis Castro', 'Proyectos', 10000000);
+
+
+INSERT INTO cargo (id_cargo,nombre_cargo,descripcion,salario) VALUES (1, 'Gerente', 'Dueño', 500000);
+INSERT INTO cargo (id_cargo,nombre_cargo,descripcion,salario) VALUES (2, 'Asistente', 'Ayuda al Grente', 250000);
+INSERT INTO cargo (id_cargo,nombre_cargo,descripcion,salario) VALUES (3, 'Desarrollador', 'El que desarrolla', 400000);
+
+                             
+
