bf4ad7a79338333523265e032806669d0bb1abac
Andres Arias <87790546+FoxJaeger007@users.noreply.github.com>
2024-11-06 21:13:38 -0500

    Update Laboratorio_5.sql
    Se realiza el punto 3 de la parte 1

--- Laboratorio_5.sql
+++ Laboratorio_5.sql
@@ -1,5 +1,5 @@
 
-/* se realizan los pasos del laboratorio 5, primera parte */
+/* se realizan los pasos del laboratorio 5, primera parte, punto 1 */
 
 
 CREATE DATABASE Empresa;
@@ -28,12 +28,47 @@ INSERT INTO cargo (id_cargo,nombre_cargo,descripcion,salario) VALUES (2, 'Asiste
 INSERT INTO cargo (id_cargo,nombre_cargo,descripcion,salario) VALUES (3, 'Desarrollador', 'El que desarrolla', 400000);
 
 
--- se realiza consulta de la tabla empleados, segunda parte
+-- se realiza consulta de la tabla empleados, punto 2
 
 
 select *  from empleados;
 
 
+-- se realiza las sentencias punto 3
+
+--	Seleccionar todos los cargos:
+
+      select *  from cargo;
+
+--	Seleccionar empleados que ganan más de 7,000,000:
+ 
+      select *  from empleados
+      where salario > 7000000;
+      
+--      Actualizar el salario de un empleado (por ejemplo, de 'Ana Gómez'): se quiere aumentar el salario de 'Ana Gómez' a 6,500,000:
+
+      update empleados
+      set salario = salario + 6500000
+      where nombre = 'Ana Gomez';
+
+-- 	Eliminar un empleado (por ejemplo, 'Luis Castro'):
+     
+      DELETE from empleados
+     where nombre = 'luis Castro';
+                                  
+                                   
+--	Seleccionar empleados con el cargo 'Desarrollador':
+      
+      select *  from empleados
+      where cargo = 'Desarrollador';
+
+--	Insertar un nuevo empleado con un cargo existente: se quiere agregar a 'Marta López' con el cargo de 'Asistente' y un salario de 3,000,000:
+
+INSERT INTO empleados (id_empl,nombre,cargo,salario) VALUES (4, 'Marta López', 'Asistente', 3000000);       
+
+  
+                                       
+                            
                                 
                              
 
